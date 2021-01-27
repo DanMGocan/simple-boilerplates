65b1101ab65b633aaabc596ee04b34409482f775
Dan Gocan <dangocan@outlook.com>
2021-01-27 16:26:25 +0000

    Added fetch() error check

---
+++ fetchErrorCheck.js
@@ -0,0 +1,15 @@
+fetch("https://api-to-call.com/endpoint")
+.then(response => {
+  if(response.ok) {
+    return response.json();
+  }
+  // executed because if the conditional is confirmed, the function will stop there but
+  // if error'ed, it will execute the throw declaration
+  throw new Error("Request failed!");
+
+}, networkError => { // executed if no response
+  console.log(networkError.message);
+})
+.then(jsonResponse => {
+  return jsonResponse;
+})
